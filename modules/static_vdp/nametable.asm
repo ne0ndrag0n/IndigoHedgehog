28d93d5d09f6f96dd46499bf39cc0e0fa35f7fc0
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2018-12-16 12:08:48 -0500

    Cleanup

--- modules/static_vdp/nametable.asm
+++ modules/static_vdp/nametable.asm
@@ -9,13 +9,6 @@ H_STATIC_VDP_NAMETABLE = 1
 ; pp pp - Root plane address
 ; aa - Tile attribute (priority (1), palette (2), vflip (1), hflip (1))
 BlitPattern:
-  ; Use formula 2( VDP_PLANE_CELLS_H * yy + xx) to get origin tile location
-  ; Compute VDP RAM write using this tile location
-
-  move.l  #$0, d0     ; clear d0 and d1
-  move.l  #$0, d1
-
-
   rts
 
  endif
